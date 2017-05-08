80ef2fc88867ad09029febce940f765f261a0658
InYub <dlsduq99@naver.com>
2017-05-08 17:19:33 +0900

    InYub

--- src/Sujin/Test.java
+++ src/Sujin/Test.java
@@ -3,9 +3,11 @@ package Sujin;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("¼öÁø");
+		System.out.println("ï¿½ï¿½ï¿½ï¿½");
 		System.out.println("test branch");
 		System.out.println("test branch2");
+		
+		System.out.println("InYub upload");
 	}
 
 }
